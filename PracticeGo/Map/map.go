b4ea71cae8dcbfa9681e5494677228f1d11a9677
SekiguchiKai <sekiguchikai@SekiguchiKai-no-MacBook-Pro.local>
2016-09-28 22:26:08 +0900

    add Map,Slice

--- PracticeGo/Map/map.go
+++ PracticeGo/Map/map.go
@@ -1,4 +1,5 @@
 //map
+//mapは[key]value
 
 package main
 
@@ -11,8 +12,22 @@ func main() {
 	staticLan[23] = "Go"
 	staticLan[75] = "TypeScript"
 	fmt.Println(staticLan)
+	fmt.Println(len(staticLan))
 
 	//リテラルでのmap生成
 	modernJS := map[string]string{"FrameWork": "Angular2", "AltJS": "TypeScript", "ESver": "ECMAScript6"}
+	fmt.Printf("%s", "modernJSの中身は")
 	fmt.Println(modernJS)
+
+	//"AltJS": "TypeScript"を削除
+	delete(modernJS, "AltJS")
+	fmt.Printf("%s", "AltJS削除後のmodernJSは")
+	fmt.Println(modernJS)
+
+	
+
+
+	
+
+
 }
--- PracticeGo/Slice/slice.go
+++ PracticeGo/Slice/slice.go
@@ -37,4 +37,37 @@ func main() {
 	s4 = append(s4, "C++", "C#", "Objective-C")
 	fmt.Println(s4)
 
+	//配列からスライスを生成する
+	//スライスは、配列への参照
+	arrayX := [6]string{"Java", "Go", "C", "JavaScript", "Python", "Ruby"}
+	sliceX := arrayX[0:3] //配列arrayXの[0]~[2]をsliceXに抽出
+	fmt.Printf("%s", "静的言語は")
+	fmt.Println(sliceX)
+
+	fmt.Printf("%s", "スライスの要素数は")
+	fmt.Println(len(sliceX))
+
+	fmt.Printf("%s", "スライスの容量は")
+	fmt.Println(cap(sliceX))
+
+	//現在の{}"Java", "Go", "C"}のスライスに要素を加える
+	fmt.Printf("%s", "現在のスライスの値は")
+	fmt.Println(sliceX)
+
+	sliceX = append(sliceX, "Lisp", "COBOL")
+	fmt.Printf("%s", "appen後のスライスの値は")
+	fmt.Println(sliceX)
+
+	//copyを使う
+	//copy先の変数を用意
+	copyx := make([]string, len(sliceX))
+	fmt.Println(copyx)
+	//copyを利用する　copyは、要素数を返すので、elementx = 5になる
+	elementx := copy(copyx, sliceX)
+	fmt.Printf("%s", "cpoyxの要素数は")
+	fmt.Println(elementx)
+
+	fmt.Printf("%s", "copyxの中身は")
+	fmt.Println(copyx)
+
 }
