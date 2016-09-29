2fd0f6faece25645cc22126d0b8e4f564308d325
SekiguchiKai <sekiguchikai@SekiguchiKai-no-MacBook-Pro.local>
2016-09-30 01:47:17 +0900

    add for

--- for/for.go
+++ for/for.go
@@ -1,28 +1,67 @@
-//for-range 配列ver
+//for.go
 
-/*
-for 配列のインデックス, 配列の要素　:= range 配列名 {
-    処理
-}
+/*Go言語での繰り返し処理は、for文のみで、while文は存在しない
+Goのfor文章は、条件式の部分に()をつかない
 */
 
 package main
 
-import (
-    "fmt"
-)
+import "fmt"
 
-func main()  {
+func main() {
 
-    //配列の生成
-programLang := [5]string{"Go", "Java", "C", "python", "Swift"}
+	fmt.Println("iのループの結果は")
+	for i := 0; i <= 5; i++ {
+		fmt.Println(i)
+	}
 
-//for分
-for i, p := range programLang {
-    fmt.Printf("program Language(%d) is %s\n", i, p )
-} 
+	/*
+	   While文をfor文で再現
+	*/
+	fmt.Println("jのループの結果は")
+	j := 0
+	for j < 5 {
+		fmt.Println(j)
+		j++
+	}
 
-return
-    
+	/*すべての条件を省略すると無限ループ*/
+
+	k := 0
+	fmt.Println("kのループの結果は")
+	for {
+		fmt.Println(k)
+		k++
+		if k == 10 {
+			break
+		}
+	}
+
+	/*break文
+	  break文以降の繰り返し処理が行われなくなる（ループを抜ける）
+	*/
+
+	//5以降の処理が行われなくなる
+	fmt.Println("lのループの結果は")
+	for l := 0; l <= 7; l++ {
+		if l == 5 {
+			break
+		}
+		fmt.Println(l)
+	}
+
+	/*
+	   continue文
+	   残り処理をスキップして次の繰り返しを始める
+	*/
+
+	//3の倍数だけ表示
+	fmt.Println("mのループの結果は")
+	for m := 1; m <= 10; m++ {
+		if m%3 != 0 {
+			continue
+		}
+		fmt.Println(m)
+	}
+
 }
-
